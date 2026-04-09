4ece537c70a08e5ef7ce4a5a8e81e4fc074f5d97
Deepika Gurrala <deepikagurrala20@gmail.com>
2026-04-09 17:33:35 +0530

    final fix all files

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI
 import asyncio
-from inference import main  # your existing logic
+from inference import main
 
 app = FastAPI()
 
 @app.post("/reset")
 async def reset():
-    return {"status": "reset done"}
+    return {"status": "reset ok"}
 
 @app.post("/step")
 async def step():
-    return {"status": "step done"}
+    return {"status": "step ok"}
 
 @app.get("/")
 def root():
     return {"message": "OpenEnv running"}
 
 @app.on_event("startup")
-async def run_inference():
+async def startup_event():
     asyncio.create_task(main())
